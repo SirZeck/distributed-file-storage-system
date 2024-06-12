20ff1a6bed2f8d6c7c9be1682e8034d906a9dd74
Adit <adithebest.ar@gmail.com>
2024-06-12 11:31:20 +0530

    Peer added

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -9,6 +9,9 @@ import (
 func TestTCPTransport(t *testing.T) {
 	listenAddr := ":4000"
 	tr := NewTCPTransport(listenAddr)
-
 	assert.Equal(t, tr.listenAddress, listenAddr)
+
+	assert.Nil(t, tr.ListenAndAccept())
+
+	select {}
 }
