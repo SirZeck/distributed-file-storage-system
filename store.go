df3efb249455df61c46a9bc204e54345843cbdac
Aditya <adithebest.ar@gmail.com>
2024-06-14 23:49:19 +0530

    CASPathTransformFunc added

--- store.go
+++ store.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-type PathTransformFunc func(string) string
+func CASPathTransformFunc(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blocksize := 5
+	sliceLen := len(hashStr) / blocksize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blocksize, (i*blocksize)+blocksize
+		paths[i] = hashStr[from:to]
+	}
+
+	return PathKey{
+		PathName: strings.Join(paths, "/"),
+		Original: hashStr,
+	}
+
+}
+
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	PathName string
+	Original string
+}
+
+func (p PathKey) Filename() string {
+	return fmt.Sprintf("%s/%s", p.PathName, p.Original)
+}
 
 type StoreOpts struct {
 	PathTransformFunc PathTransformFunc
@@ -27,13 +60,12 @@ func NewStore(opts StoreOpts) *Store {
 }
 
 func (s *Store) writeStream(key string, r io.Reader) error {
-	pathName := s.PathTransformFunc(key)
-	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+	pathKey := s.PathTransformFunc(key)
+	if err := os.MkdirAll(pathKey.PathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	filename := "somefilename"
-	pathAndFilename := pathName + "/" + filename
+	pathAndFilename := pathKey.Filename()
 
 	f, err := os.Create(pathAndFilename)
 	if err != nil {
--- store_test.go
+++ store_test.go
@@ -5,9 +5,23 @@ import (
 	"testing"
 )
 
+func TestPathTransformFunc(t *testing.T) {
+	key := "momsbestpicture"
+	pathKey := CASPathTransformFunc(key)
+	expectedOriginalKey := "6804429f74181a63c50c3d81d733a12f14a353ff"
+	expectedPathname := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+	if pathKey.PathName != expectedPathname {
+		t.Errorf("have %s want %s", pathKey.PathName, expectedPathname)
+	}
+	if pathKey.Original != expectedOriginalKey {
+		t.Errorf("have %s want %s", pathKey.Original, expectedOriginalKey)
+	}
+
+}
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	s := NewStore(opts)
 
